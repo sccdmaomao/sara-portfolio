ab5aa7caeeb76bbd1e1a8a154ead24dbb8c9e46a
guohao.yan <sccdmaomao@hotmail.com>
2023-04-13 13:08:58 -0400

    init

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'react-app',
+        'plugin:react/recommended',
+        'standard-with-typescript',
+        'prettier',
+    ],
+    overrides: [],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        project: ['./tsconfig.json'],
+    },
+    plugins: ['react', 'prettier', 'simple-import-sort'],
+    rules: {
+        // react
+        'react/react-in-jsx-scope': 0,
+        'react/prop-types': 0,
+        // simple-import-sort
+        'simple-import-sort/imports': 'error',
+        'simple-import-sort/exports': 'error',
+        'import/newline-after-import': 'error',
+        'import/no-duplicates': 'error',
+        'import/first': 'error',
+        'prettier/prettier': [
+            'error',
+            {
+                endOfLine: 'auto',
+            },
+        ],
+        // typescript
+        '@typescript-eslint/explicit-function-return-type': 0,
+        '@typescript-eslint/strict-boolean-expressions': 0,
+        '@typescript-eslint/no-floating-promises': 0,
+        '@typescript-eslint/no-unused-vars': 1,
+        'promise/param-names': 0,
+    },
+}
---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+    return <div className="App">sara portfolio app</div>
+}
+
+export default App
