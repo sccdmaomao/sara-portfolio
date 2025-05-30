3dd676efd722c4fe288e8c585fc77780468c0355
guohao.yan <sccdmaomao@hotmail.com>
2025-05-29 23:42:58 -0400

    add rsbuild, revive project

---
+++ rsbuild.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from '@rsbuild/core'
+import { pluginReact } from '@rsbuild/plugin-react'
+
+export default defineConfig({
+    plugins: [pluginReact()],
+    source: {
+        entry: {
+            index: './src/index.tsx',
+        },
+    },
+    html: {
+        template: './public/index.html',
+    },
+    dev: {
+        hmr: true,
+        liveReload: true,
+        progressBar: true,
+        cliShortcuts: true,
+    },
+    server: {
+        port: 3000,
+    },
+    output: {
+        distPath: {
+            root: 'dist',
+            js: 'static/js',
+            css: 'static/css',
+            assets: 'static/assets',
+        },
+    },
+})
